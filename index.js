3e31b6c069c7039a88dec164b82e2744a63bea7a
rayane.melo <rayane.melo@lighthouseit.com.br>
2022-06-07 16:08:07 -0300

    finish

--- index.js
+++ index.js
@@ -1,44 +1,79 @@
-n1 = document.querySelector("#n1");
-n2 = document.querySelector("#n2");
-op = document.querySelector("#op");
+const display = document.getElementById("display");
+const buttons = document.querySelectorAll(".key");
+let n1 = 0;
+let n2 = 0;
+let operator = "";
+let result = 0;
+let isOperator = false;
+let isDot = false;
 
-switch (op) {
-  case "+":
-    soma(n1, n2);
-    break;
-  case "-":
-    subt(n1, n2);
-    break;
-  case "/":
-    divisao(n1, n2);
-  case "*":
-    mult(n1, n2);
-  case "^2":
-    potencia(n1, n2);
-  case "√":
-    raiz(n1);
-}
+buttons.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    let key = e.target.innerText;
+    if (key >= "0" && key <= "9") {
+      if (isOperator) {
+        display.value = key;
+        isOperator = false;
+      } else {
+        if (display.value === "0") {
+          display.value = key;
+        } else {
+          display.value += key;
+        }
+      }
+    }
 
-const soma = (n1, n2) => {
-  return n1 + n2;
-};
+    if (key === "+" || key === "-" || key === "x" || key === "/") {
+      if (isOperator) {
+        isOperator = false;
+      } else {
+        n1 = Number(display.value);
+        operator = key;
+        isOperator = true;
+      }
+    }
 
-const subt = (n1, n2) => {
-  return n1 - n2;
-};
+    if (key === "C") {
+      display.value = "0";
+      isOperator = false;
+      isDot = false;
+      n1 = 0;
+      n2 = 0;
+      operator = "";
+    }
 
-const divisao = (n1, n2) => {
-  return n1 / n2;
-};
+    if (key === "x²") {
+      display.value = Math.pow(display.value, 2);
+    }
 
-const mult = (n1, n2) => {
-  return n1 * n2;
-};
+    if (key === "√") {
+      display.value = Math.sqrt(display.value);
+    }
 
-const potencia = (n1, n2) => {
-  return Math.pow(n1, 2);
-};
+    if (key === ".") {
+      if (!isDot) {
+        display.value += key;
+        isDot = true;
+      }
+    }
 
-const raiz = (n1) => {
-  return Math.sqrt(n1);
-};
+    if (key === "=") {
+      n2 = Number(display.value);
+      if (operator === "+") {
+        result = n1 + n2;
+      } else if (operator === "-") {
+        result = n1 - n2;
+      } else if (operator === "x") {
+        result = n1 * n2;
+      } else if (operator === "/") {
+        result = n1 / n2;
+      }
+      display.value = result;
+      isOperator = false;
+      isDot = false;
+      n1 = 0;
+      n2 = 0;
+      operator = "";
+    }
+  });
+});
